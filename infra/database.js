2855b337dfd75fd63148a9e8d443612d2c17fc80
Ismael Silva <ismaelsilva@icloud.com>
2025-12-30 23:02:41 +1300

    handles database `ssl` connection

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,21 @@
 import { Client } from "pg";
 
+const isLocal =
+  process.env.POSTGRES_HOST === "localhost" ||
+  process.env.POSTGRES_HOST === "127.0.0.1" ||
+  process.env.IS_LOCAL === "true";
+
 async function query(queryObject) {
   const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  console.log("Postgres Credentials:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
